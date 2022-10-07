79d67e9f0f3252a3e3d261dbd1bbbcbee7391803
rybaaa <bolshayaryba22@gmail.com>
2022-10-07 23:48:06 +0300

    super-button/input/checkbox added

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,11 +17,9 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
+    const finalClassName = s.button +
+        (disabled ? s.disabled : '') +
+        (xType === 'red' ? s.red : '')
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
