914026ae251600520753afa55dbcd32cd9d53349
rybaaa <bolshayaryba22@gmail.com>
2022-10-08 16:49:32 +0300

    fix default button

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -19,8 +19,8 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
 ) => {
     const finalClassName = s.button +
         (disabled ? ' ' + s.disabled : '') +
-        (xType === 'red' ? ' ' + s.red : ' ' + s.secondary)+
-        + (className ? ' ' + s.className : '') // задачка на смешивание классов
+        (xType === 'red' ? ' ' + s.red : xType === 'secondary' ? ' ' + s.secondary: ' ' + s.default)
+        // + (className ? ' ' + s.className : '') // задачка на смешивание классов
 
     return (
         <button
