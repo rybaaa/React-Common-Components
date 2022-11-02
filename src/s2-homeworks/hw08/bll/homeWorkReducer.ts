956563374ee7779c48b7f4120ba741d53eb58e24
rybaaa <bolshayaryba22@gmail.com>
2022-11-02 20:42:31 +0300

    hw8 fix

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,13 +8,15 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': { // by name
             if (action.payload === 'up') {
-                return state.sort((a,b)=>a.name.localeCompare(b.name))
-            } else {
-                return state.sort((a,b)=>b.name.localeCompare(a.name))
+                return state.sort((a, b) => a.name.localeCompare(b.name))
             }
+            if (action.payload === 'down') {
+                return state.sort((a, b) => b.name.localeCompare(a.name))
+            }
+            return state
         }
         case 'check': {
-            return state.filter(u=>u.age>=action.payload) // need to fix
+            return state.filter(u => u.age >= action.payload).reverse() // need to fix
         }
         default:
             return state
