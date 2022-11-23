791924b0726e9a3a38eadc988e0845e8941294ef
rybaaa <bolshayaryba22@gmail.com>
2022-11-24 00:19:29 +0300

    hw11 tange slider

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,9 +4,7 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
+            sx={{ width: 300 }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
